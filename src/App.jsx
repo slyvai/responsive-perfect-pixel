69d43857c6d32eb14410f9fb5b00b9bc48f94451
slyvia <slyviafams@gmail.com>
2025-04-16 11:20:54 +0700

    third-commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,31 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="background">
-        <div className="background2">
+      <div className="background ">
+        <div className="background2 pc">
           <div className='text'>
             <h2>Kecerdasan Visual</h2>
             <h1>Lihat dunia dari lensa yang baru.</h1>
             <p>
               Gunakan <span className='highlight'>kecerdasan visual</span> melalui Kontrol Kamera untuk langsung
+              mempelajari tentang semua hal yang Anda liat. Cukup <span className='highlight'>arahkan iPhone
+              16 Pro Anda untuk menemukan lebih</span> atau berinteraksi dengan sesuatu
+              di depan Anda. Cari tahu di mana tempat membeli barang baru yang
+              Anda lihat, identifikasikan tanaman dan hewan, dan banyak lagi.
+            </p>
+              </div>
+              <div className='image'>
+              <img src={Image1} alt="Visual Intelligence" />
+            </div>
+          </div>
+
+
+          <div className="background2 mb">
+          <div className='text'>
+            <h2>Kecerdasan Visual</h2>
+            <h1>Lihat dunia dari <br /> lensa yang baru.</h1>
+            <p>
+              Gunakan <span className='highlight'>kecerdasan visual</span> melalui Kontrol Kamera <br /> untuk langsung 
               mempelajari tentang semua hal yang Anda liat. Cukup <span className='highlight'>arahkan iPhone
               16 Pro Anda untuk menemukan lebih</span> atau berinteraksi dengan sesuatu
               di depan Anda. Cari tahu di mana tempat membeli barang baru yang
